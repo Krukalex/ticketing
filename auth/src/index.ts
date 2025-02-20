553a195e917191fc32e7c2b2c2c6fb7ec541e168
krukalex <krukalex99@gmail.com>
2025-02-20 18:47:10 -0500

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI must be defined");
+    }
+    await mongoose.connect(process.env.MONGO_URI);
+    app.listen(3000, () => {
+      console.log("listening on port 3000");
+    });
+    console.log("connected to mongodb");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
